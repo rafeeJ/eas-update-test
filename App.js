0dfece27530ae049b8d281c0ed3267e947f7c7f6
Rafee Jenkins <rafeejenkins@gmail.com>
2022-12-23 15:10:36 +0000

    Adds a counter
    * feat: updates text
    
    * chore: updates to eas.json
    
    * feat: adds counter

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is an update published from GH actions</Text>
+      <Text>Count: {count}</Text>
+      <Button title='Press me' onPress={() => setCount(count + 1)} />
+      <Button title='Reset' onPress={() => setCount(0)} />
       <StatusBar style="auto" />
     </View>
   );
